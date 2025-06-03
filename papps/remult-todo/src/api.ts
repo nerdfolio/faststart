3e3723b0b83959b59ac18afb4e68126fbc25543d
taivo <tai@localhost>
2025-06-03 00:05:01 -0700

    add a dev helper for remult-d1-http to simplify wrangler config reading

--- papps/remult-todo/src/api.ts
+++ papps/remult-todo/src/api.ts
@@ -1,4 +1,4 @@
-import { createD1DataProviderWithCredentials } from "remult-d1/remult-d1-http"
+import { devCreateD1DataProviderWithCredentials } from "remult-d1/remult-d1-http"
 import { remultApi } from "remult/remult-solid-start"
 import { getUser } from "./auth"
 import { Task } from "./shared/Task"
@@ -10,10 +10,11 @@ export const api = remultApi({
 	entities: [Task],
 	controllers: [TasksController],
 	getUser,
-	dataProvider: createD1DataProviderWithCredentials({
+	dataProvider: devCreateD1DataProviderWithCredentials({
 		accountId: process.env.CLOUDFLARE_ACCOUNT_ID ?? "",
 		apiToken: process.env.CLOUDFLARE_D1_TOKEN ?? "",
-		databaseId: "af1e900f-06b9-4d71-9ed7-78639cd7dc79",
+		//databaseId: "af1e900f-06b9-4d71-9ed7-78639cd7dc79",
+		bindingName: "DB",
 	}),
 	// dataProvider: new SqlDatabase(new BetterSqlite3DataProvider(new Database("./db/testdb.sqlite"))),
 	admin: true,
--- plibs/remult-d1/src/remult-d1-http.ts
+++ plibs/remult-d1/src/remult-d1-http.ts
@@ -2,24 +2,45 @@ import Cloudflare from "cloudflare"
 import { SqlDatabase } from "remult"
 import { type D1Client, D1DataProvider, type D1RowObject } from "./remult-d1"
 
-type D1Credentials = { accountId: string; databaseId: string; apiToken: string }
+type D1Credentials = { accountId: string; apiToken: string; databaseId: string }
 export function createD1DataProviderWithCredentials(creds: D1Credentials) {
 	return new SqlDatabase(new D1DataProvider(new D1HttpClient(creds)))
 }
 
-export class D1HttpClient implements D1Client {
-	/*
-	 * Simple d1 client that internally uses a cloudflare sdk client along with
-	 * d1 credentials to access D1 remotely
-	 *
-	 * const d1 = new D1HttpClient({
-	 * 	accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
-	 * 	apiToken: process.env.CLOUDFLARE_D1_TOKEN,
-	 *   databaseId: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-	 * })
-	 *
-	 */
 
+/*
+ * The most frequent use case of d1-http is for local development or for scripts called locally.
+ * For those cases, it's annoying to have to worry about copying the correct databaseId from wrangler config.
+ * This function is a dev-mode helper that uses wrangler to parse its config file and give us the databaseId from
+ * the binding name. This function also simplifies account_id in a similar way
+ */
+export async function devCreateD1DataProviderWithCredentials({ accountId, apiToken, databaseId, bindingName }: { accountId?: string, apiToken: string, databaseId?: string, bindingName?: string }) {
+	if (accountId && databaseId) {
+		return createD1DataProviderWithCredentials({ accountId, apiToken, databaseId })
+	}
+
+	if (!databaseId && !bindingName) {
+		throw new Error("Either databaseId or bindingName must be provided")
+	}
+
+	const { unstable_readConfig } = await import("wrangler")
+	const { account_id, d1_databases } = unstable_readConfig({})
+
+	const account = accountId ?? account_id ?? process.env.CLOUDFLARE_ACCOUNT_ID
+	if (!account) {
+		throw new Error("accountId not specified in wrangler config or in CLOUDFLARE_ACCOUNT_ID env. Please specify it explicitly")
+	}
+
+	const db = d1_databases.find((d) => d.binding === bindingName)?.database_id
+	if (!db) {
+		throw new Error(`Could not find database_id for d1 binding ${bindingName}. Please check wrangler config file`)
+	}
+
+	return createD1DataProviderWithCredentials({ accountId: account, apiToken, databaseId: db ?? "" })
+}
+
+
+export class D1HttpClient implements D1Client {
 	#d1: Cloudflare["d1"]["database"]
 	#accountId: string
 	#databaseId: string
@@ -31,18 +52,17 @@ export class D1HttpClient implements D1Client {
 	}
 
 	async execute(sql: string, params?: unknown[]) {
-
 		return this.#d1.query(this.#databaseId, {
 			sql,
 			params: params as string[],
 			account_id: this.#accountId,
 		}).then(({ result: pages }) => pages.flatMap((page) => page.results as D1RowObject[]))
 
-		// // // NOTE: d1 query endpoint returns the result as array of object with keys as column names.
-		// // // We're returning that now because the rest of remult seems to expect that.
-		// // // D1 also has a more efficient raw() endpoint that returns {columns: string[], rows: any[][]} that we may want
-		// // // to use eventually, and write code like fromRowToSql()
-		// // // at https://github.com/tursodatabase/libsql-client-ts/blob/main/packages/libsql-client/src/sqlite3.ts#L398
-		// // // to adapt the data
+		// NOTE: d1 query endpoint returns the result as array of object with keys as column names.
+		// We're returning that now because the rest of remult seems to expect that.
+		// D1 also has a more efficient raw() endpoint that returns {columns: string[], rows: any[][]} that we may want
+		// to use eventually, and write code like fromRowToSql()
+		// at https://github.com/tursodatabase/libsql-client-ts/blob/main/packages/libsql-client/src/sqlite3.ts#L398
+		// to adapt the data
 	}
 }
