730180f88cca7a5c425d51ff8484ec50a77f8b74
Tai Vo <tai.a.vo@gmail.com>
2025-06-18 18:25:32 -0700

    wip: remaking package structure

--- database/schema/main-schema.ts
+++
@@ -1,6 +0,0 @@
-import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
-
-export const exampleTable = sqliteTable("example", {
-	id: int().primaryKey({ autoIncrement: true }),
-	content: text().notNull()
-})
