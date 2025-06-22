745c6b091b2cbe079e9b92326a36f2d5c06ce75b
Tai Vo <tai.a.vo@gmail.com>
2025-06-22 02:23:38 -0700

    comments

--- fastcore/utils/auth-gen.ts
+++ fastcore/utils/auth-gen.ts
@@ -1,7 +1,8 @@
 //
 // Export "auth" so remult-better-auth config-parser will pick it up for auth schema generation.
-// This "auth" object is only used for that. Actual auth configuration should be done in
-// individual apps where actual data provider bindings are available
+// This "auth" object is only used for that.
+//
+// Actual auth configuration should be done in individual apps where actual data provider bindings are available
 //
 
 import { remultAdapter } from "@nerdfolio/remult-better-auth"
