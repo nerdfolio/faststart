0c09d07921be7512953656adbaccf5a46e9f3512
Tai Vo <tai.a.vo@gmail.com>
2025-06-22 00:37:54 -0700

    add organization plugins for better-auth

--- fastcore/auth/config.ts
+++ fastcore/auth/config.ts
@@ -1,25 +1,21 @@
 import { type BetterAuthOptions, betterAuth } from "better-auth"
-import { admin, magicLink } from "better-auth/plugins"
+import { admin, magicLink, organization } from "better-auth/plugins"
 import { logMagicLinkToServerConsole } from "./handlers/magic-link"
 
-type BetterAuthSchemaOptions = Pick<BetterAuthOptions, "user" | "account" | "session" | "verification">
-export const coreBetterAuthSchema: BetterAuthSchemaOptions = {
+export const coreBetterAuthConfig: BetterAuthOptions = {
 	user: {
 		additionalFields: {
 			theme: { type: "string" },
 			isTest: { type: "boolean", defaultValue: false },
 		},
 	},
-} as const
-
-export const coreBetterAuthConfig: BetterAuthOptions = {
-	...coreBetterAuthSchema,
 	plugins: [
 		magicLink({
 			sendMagicLink: logMagicLinkToServerConsole,
 			//disableSignUp: true
 		}),
 		admin(),
+		organization(),
 	],
 } as const
 
@@ -32,11 +28,3 @@ export function initBetterAuth(opts: BetterAuthOptions) {
 		plugins,
 	})
 }
-
-
-//
-// Export "auth" so remult-better-auth config-parser will pick it up for auth schema generation.
-// This "auth" object is only used for that. To fully configure better-auth, we
-// need to create an "auth.ts" file and call betterAuth({ ...coreBetterAuthSchema, .... })
-// with the proper database adapter.
-// export const auth = { options: coreBetterAuthConfig }
---
+++ fastcore/utils/auth-gen.ts
@@ -0,0 +1,14 @@
+//
+// Export "auth" so remult-better-auth config-parser will pick it up for auth schema generation.
+// This "auth" object is only used for that. Actual auth configuration should be done in
+// individual apps where actual data provider bindings are available
+//
+
+import { remultAdapter } from "@nerdfolio/remult-better-auth"
+import { Remult } from "remult"
+import { initBetterAuth } from "../auth/config"
+
+// with the proper database adapter.
+export const auth = initBetterAuth({
+	database: remultAdapter(new Remult(), { authEntities: {} }),
+})
