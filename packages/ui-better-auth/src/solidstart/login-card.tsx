8f4b92251f0f61872f372bc6e276c88fa7208cae
Tai Vo <tai.a.vo@gmail.com>
2025-06-24 23:24:25 -0700

    should login card be generic?

--- packages/ui-better-auth/src/solidstart/login-card.tsx
+++ packages/ui-better-auth/src/solidstart/login-card.tsx
@@ -1,9 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@nerdfolio/ui-base-solid/ui"
 import { cn } from "@nerdfolio/ui-base-solid/utils"
 import { A } from "@solidjs/router"
-import { type ComponentProps, createSignal, Show, splitProps } from "solid-js"
-import EmailPasswordForm from "./forms/emailpassword-form"
-import MagicLinkForm from "./forms/magiclink-form"
+import { type ComponentProps, splitProps } from "solid-js"
 import type { BetterAuthClient } from "./types"
 
 export default function LoginCard(
@@ -14,14 +12,12 @@ export default function LoginCard(
 		emailPassword?: boolean
 	}
 ) {
-	const [local, rest] = splitProps(props, ["class", "magicLink", "emailPassword"])
+	const [local, rest] = splitProps(props, ["class", "authClient", "callbackUrl"])
 
-	const { magicLink = true, emailPassword = false } = local
+	// const { magicLink = true, emailPassword = false } = local
 
-	const [errorMsg, setErrorMsg] = createSignal("")
-	const [successMsg, setSuccessMsg] = createSignal("")
-
-	console.log("props", props)
+	// const [errorMsg, setErrorMsg] = createSignal("")
+	// const [successMsg, setSuccessMsg] = createSignal("")
 
 	return (
 		<div class={cn("flex flex-col gap-6", local.class)} {...rest}>
@@ -31,7 +27,19 @@ export default function LoginCard(
 				</CardHeader>
 
 				<CardContent>
-					<div class="flex flex-col gap-12">
+					<div class="flex flex-col gap-12">--</div>
+				</CardContent>
+			</Card>
+
+			<div class="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+				By continuing, you agree to our <A href="/terms">Terms of Service</A> and <A href="/privacy">Privacy Policy</A>.
+			</div>
+		</div>
+	)
+}
+
+/*
+
 						<Show when={emailPassword}>
 							<EmailPasswordForm
 								authClient={props.authClient}
@@ -48,16 +56,10 @@ export default function LoginCard(
 								setSuccessMsg={setSuccessMsg}
 							/>
 						</Show>
-					</div>
-					<div>
+
+						<div>
 						{errorMsg() ? <p class="text-sm text-red-500 pt-2">{errorMsg()}</p> : null}
 						{successMsg() ? <p class="text-sm text-green-500 pt-2">{successMsg()}</p> : null}
 					</div>
-				</CardContent>
-			</Card>
-			<div class="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-				By continuing, you agree to our <A href="/terms">Terms of Service</A> and <A href="/privacy">Privacy Policy</A>.
 			</div>
-		</div>
-	)
-}
+*/
