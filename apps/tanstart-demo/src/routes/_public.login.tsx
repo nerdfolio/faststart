fa541910570da2c14ac75a14a4ffa4ac5efce6ac
Tai Vo <tai.a.vo@gmail.com>
2025-07-12 00:01:43 -0700

    clean up dependencies for ui

--- apps/tanstart-demo/src/routes/_public.login.tsx
+++ apps/tanstart-demo/src/routes/_public.login.tsx
@@ -1,9 +1,15 @@
+import { LoginCard } from "@nerdfolio/solid-better-auth"
+import { GuestListForm } from "@nerdfolio/solid-better-auth/forms"
 import { createFileRoute } from "@tanstack/solid-router"
 
 export const Route = createFileRoute("/_public/login")({
-	component: About,
+	component: Login,
 })
 
-function About() {
-	return <h1>Roadmap</h1>
+function Login() {
+	return (
+		<LoginCard>
+			<GuestListForm />
+		</LoginCard>
+	)
 }
--- packages/solid-better-auth/src/forms/index.tsx
+++ packages/solid-better-auth/src/forms/index.tsx
@@ -1,1 +1,3 @@
+export * from "./emailpassword-form"
 export * from "./guestlist-form"
+export * from "./magiclink-form"
